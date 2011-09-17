9eabdcd686e92c2d881fb5d12d3164e5d398de7e
leod <subtle.frustration@proton.me>
2011-09-17 18:02:20 +0200

    Use unix-style line delimiters.

--- src/jlogic/Atom.java
+++ src/jlogic/Atom.java
@@ -1,5 +1,16 @@
 package jlogic;
 
-public final class Atom {
-	
+public final class Atom implements Term {
+    private String name;
+
+    public Atom(String name) {
+        if (name == null)
+            throw new IllegalArgumentException("name must not be null");
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
