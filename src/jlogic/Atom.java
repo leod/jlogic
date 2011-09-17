a2e40cddfaefe2a894d2f426129eec2ee475d5b3
leod <subtle.frustration@proton.me>
2011-09-17 18:11:44 +0200

    Make Atom immutable. Check for name validity.

--- src/jlogic/Atom.java
+++ src/jlogic/Atom.java
@@ -1,16 +1,23 @@
 package jlogic;
 
 public final class Atom implements Term {
-    private String name;
+    private final String name;
 
     public Atom(String name) {
         if (name == null)
             throw new IllegalArgumentException("name must not be null");
+        if (!isValidName(name))
+            throw new IllegalArgumentException("name is invalid");
 
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private static boolean isValidName(String name) {
+        // TODO: Implement isValidName.
+        return true;
     }
 }
