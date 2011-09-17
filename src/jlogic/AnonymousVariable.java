f27d076d6d30a1fc667d0b570eb6f40e2c482b9f
leod <subtle.frustration@proton.me>
2011-09-18 01:21:33 +0200

    Add AnonymousVariable class.

---
+++ src/jlogic/AnonymousVariable.java
@@ -0,0 +1,20 @@
+package jlogic;
+
+public final class AnonymousVariable implements Term {
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null)
+            return false;
+        if (getClass() != object.getClass())
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "_";
+    }
+}
