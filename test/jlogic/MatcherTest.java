9c5fb36088a87ae4dd84ce3ec0ea686f3e28dbc6
leod <subtle.frustration@proton.me>
2011-09-18 02:08:52 +0200

    Start using JUnit.

---
+++ test/jlogic/MatcherTest.java
@@ -0,0 +1,12 @@
+package jlogic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatcherTest {
+    @Test
+    public void testMatch() {
+        fail("Not yet implemented");
+    }
+}
