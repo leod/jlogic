c19ddd069546cdcaf5955183c9c376500c11ac16
leod <subtle.frustration@proton.me>
2011-09-18 02:09:00 +0200

    Add matcher test program.

--- src/jlogic/Main.java
+++ src/jlogic/Main.java
@@ -2,6 +2,14 @@ package jlogic;
 
 public final class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Frame frame = new Frame();
+
+        Term left;
+        Term right;
+
+        left = new Structure(new Atom("a"), new Term[] { new Atom("b"), new Variable("X") });
+        right = new Structure(new Atom("a"), new Term[] { new Atom("b"), new Structure(new Atom("c"), new Term[] { new Variable("Y"), new Atom("e") }) });
+
+        System.out.println(Matcher.match(frame, left, right));
     }
 }
