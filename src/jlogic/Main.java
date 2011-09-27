d1ba2828cc90ea7d158e86eeea15fdfb3121de30
leod <subtle.frustration@proton.me>
2011-09-27 22:39:42 +0200

    Add help messages and tree image generation to the REPL.

--- src/jlogic/Main.java
+++ src/jlogic/Main.java
@@ -3,30 +3,16 @@ package jlogic;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.StringReader;
 
 import jlogic.read.Lexer;
 import jlogic.read.Parser;
 import jlogic.read.ReadException;
-import jlogic.term.Term;
 
 public final class Main {
     public static void main(String[] args) throws ReadException, IOException, InterruptedException {
         Knowledge knowledge = readFile("test.jl");
-        REPL repl = new REPL(knowledge, System.in, System.out, System.err);
+        REPL repl = new REPL(knowledge, System.in, System.out);
         repl.run();
-
-        // DOT dot = new DOT(search);
-        // createGraphImage("searchgraph.png", dot.generateDOT());
-    }
-
-    private static Term termFromString(String string) throws ReadException, IOException {
-        final String file = "[main" + string.hashCode() + "]";
-        Lexer lexer = new Lexer(file, new StringReader(string));
-        Parser parser = new Parser(lexer);
-
-        return parser.parseTerm();
     }
 
     private static Knowledge readFile(String path) throws ReadException, IOException {
@@ -42,18 +28,4 @@ public final class Main {
         }
     }
 
-    private static void createGraphImage(String filename, String dot) throws IOException, InterruptedException {
-        // This might not always work, see
-        // <http://www.javaworld.com/javaworld/jw-12-2000/jw-1229-traps.html?page=4>.
-
-        Process process = Runtime.getRuntime().exec("dot -o" + filename + " -Tpng");
-        OutputStream stdin = process.getOutputStream();
-        stdin.write(dot.getBytes());
-        stdin.flush();
-        stdin.close();
-        process.waitFor();
-
-        if (process.exitValue() != 0)
-            throw new IOException("dot returned " + process.exitValue());
-    }
 }
--- src/jlogic/REPL.java
+++ src/jlogic/REPL.java
@@ -10,6 +10,7 @@ import java.io.StringReader;
 import java.io.Writer;
 import java.util.Map;
 
+import jlogic.interpret.DOT;
 import jlogic.interpret.Frame;
 import jlogic.interpret.SearchTree;
 import jlogic.read.Lexer;
@@ -20,61 +21,72 @@ import jlogic.term.Term;
 import jlogic.term.Variable;
 
 public final class REPL {
-    private final Knowledge knowledge;
+    private final String helpString = "" +
+            "Input a query in the form of a valid term (for example foo(Bar), or bla(blub, X)).\n" +
+            "The query will be evaluated against the knowledge base and valid solutions for the free variables in the query will be listed one by one.\n" +
+            "Additionally, special commands prefixed by a colon can be issued.\n\n" +
+            "Special commands:\n" +
+            "\t:quit\t\t\tStop the REPL.\n" +
+            "\t:help\t\t\tPrint this help.\n" +
+            "\t:consult file\t\tLoad rules and facts from `file'.\n" +
+            "\t:knowledge\t\tPrint all known rules and facts.\n" +
+            "\t:savetree `file'\tCreate a PNG image named `file' containing the search tree of the last query.\n" +
+            "\t\t\t\tIf no `file' is supplied, the default name `searchtree.png' is assumed.\n";
+
+    private Knowledge knowledge;
 
     private final BufferedReader input;
     private final Writer output;
-    private final Writer errorOutput;
 
-    public REPL(Knowledge knowledge, InputStream input, OutputStream output,
-            OutputStream errorOutput) {
+    private SearchTree lastSearchTree = null;
+    private boolean quitLoop = false;
+
+    public REPL(Knowledge knowledge, InputStream input, OutputStream output) {
         this.knowledge = knowledge;
         this.input = new BufferedReader(new InputStreamReader(input));
         this.output = new OutputStreamWriter(output);
-        this.errorOutput = new OutputStreamWriter(errorOutput);
     }
 
     public void run() throws IOException {
-        while (true) {
+        while (!quitLoop) {
             Structure goal = read();
             if (goal == null)
-                return;
-
-            SearchTree searchTree = new SearchTree(knowledge, goal);
-
-            Frame frame;
-            String line;
-            do {
-                frame = searchTree.searchOne();
-                if (frame != null) {
-                    if (frame.getInstantiations().size() > 0) {
-                        for (Map.Entry<Variable, Term> entry : frame.getInstantiations().entrySet()) {
-                            output.write(entry.getKey().toString());
-                            output.write(" = ");
-                            output.write(entry.getValue().toString());
-                            output.write("\n");
-                        }
-                    } else
-                        output.write("Yes.\n");
-                }
-                else
-                    output.write("No.\n");
-                output.flush();
-
-                if (frame == null)
-                    break; // That was the last solution
-
-                output.write("More? [y]/n: ");
-                output.flush();
-                line = input.readLine().trim();
-
-                output.write("\n");
-            } while (line.equals("") || line.equals("Y") || line.equals("y")
-                    || line.equals("yes") || line.equals(";"));
-
-            output.write("\n");
-            output.flush();
+                continue;
+            evaluateAndPrint(goal);
         }
+    }
+
+    private void evaluateAndPrint(Structure goal) throws IOException {
+        SearchTree searchTree = new SearchTree(knowledge, goal);
+
+        Frame frame;
+        String line = null;
+        boolean writeAll = false;
+        do {
+            frame = searchTree.searchOne();
+            writeInstantiations(frame);
+
+            if (frame == null)
+                break; // That was the last solution
+
+            if (writeAll)
+                continue;
+
+            output.write("More? [y]/n/all: ");
+            output.flush();
+            line = input.readLine().trim();
+            output.write("\n");
+
+            if (line.equals("all"))
+                writeAll = true;
+        } while (writeAll
+                || line.equals("") || line.equals("Y") || line.equals("y")
+                || line.equals("yes") || line.equals(";"));
+
+        output.write("\n");
+        output.flush();
+
+        lastSearchTree = searchTree;
     }
 
     // TODO: Read multiple goals at once
@@ -83,15 +95,20 @@ public final class REPL {
             output.write("?- ");
             output.flush();
 
-            String line = input.readLine();
+            String line = input.readLine().trim();
+
+            if (line.equals("")) {
+                // Don't create useless parser error messages on empty input
+                return null;
+            }
 
             if (line.endsWith("\n"))
                 line = line.substring(0, line.length() - 1);
 
             if (line.startsWith(":")) {
                 String command = line.substring(1);
-                if (command.equals("quit"))
-                    return null;
+                handleSpecialCommand(command);
+                return null;
             }
 
             try {
@@ -100,8 +117,85 @@ public final class REPL {
 
                 return parser.parseStructure();
             } catch (ReadException exception) {
-                errorOutput.write(exception.toString());
+                output.write("Syntax error: " + exception.getMessage() + ".");
+                output.write("\n\n");
             }
+        }
+    }
+
+    private void handleSpecialCommand(String command) throws IOException {
+        String[] arguments = command.split("\\s+");
+
+        if (arguments.length < 1) {
+            output.write("No command name supplied.");
+            output.flush();
+            return;
+        }
+
+        if (arguments[0].equals("quit"))
+            quitLoop = true;
+        else if (arguments[0].equals("help"))
+            output.write(helpString);
+        else if (arguments[0].equals("consult"))
+            output.write("Not implemented yet.");
+        else if (arguments[0].equals("knowledge"))
+            output.write(knowledge.toString());
+        else if (arguments[0].startsWith("savetree")) {
+            if (lastSearchTree != null) {
+                String filename;
+                if (arguments.length == 1)
+                    filename = "searchtree.png";
+                else
+                    filename = arguments[1];
+
+                DOT dot = new DOT(lastSearchTree);
+                createGraphImage(filename, dot.generateDOT());
+
+                output.write("Saved tree to " + filename);
+            } else {
+                output.write("No query was issued yet.");
+            }
+        } else
+            output.write("Unknown command: " + command);
+
+        output.write("\n");
+        output.flush();
+    }
+
+    private void writeInstantiations(Frame frame) throws IOException {
+        if (frame != null) {
+            if (frame.getInstantiations().size() > 0) {
+                for (Map.Entry<Variable, Term> entry : frame.getInstantiations().entrySet()) {
+                    output.write(entry.getKey().toString());
+                    output.write(" = ");
+                    output.write(entry.getValue().toString());
+                    output.write("\n");
+                }
+            } else
+                output.write("Yes.\n");
+        }
+        else
+            output.write("No.\n");
+        output.flush();
+    }
+
+    private static void createGraphImage(String filename, String dot) throws IOException {
+        // This might not always work, see
+        // <http://www.javaworld.com/javaworld/jw-12-2000/jw-1229-traps.html?page=4>.
+
+        try {
+            Process process = Runtime.getRuntime().exec("dot -o" + filename + " -Tpng");
+            OutputStream stdin = process.getOutputStream();
+            stdin.write(dot.getBytes());
+            stdin.flush();
+            stdin.close();
+            process.waitFor();
+
+            if (process.exitValue() != 0)
+                throw new IOException("dot returned " + process.exitValue());
+        } catch (InterruptedException exception) {
+            // TODO: Need to find out when this happens and what to do
+            throw new IOException(exception.toString());
         }
     }
 }
